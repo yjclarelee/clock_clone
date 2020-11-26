7cfb0d5b614bdee08e95730cccb3ce6e4b412b71
yjclarelee <yjclarelee@postech.ac.kr>
2020-11-27 07:00:31 +0900

    Add comments to understand greeting.js

--- todo.js
+++ todo.js
@@ -6,7 +6,7 @@ const TO_DO_LOCAL_STORAGE = "toDos";
 
 function paintToDo(text){
     // can't you just append li without span?
-    console.log(text);
+    // span is a container to put the text into
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
@@ -22,13 +22,17 @@ function saveToDo(){
 }
 
 function handleSubmit(event){
+    // check what the function of the next line is
     event.preventDefault();
+    // paint the input value
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
+    // remove the text after input
     toDoInput.value = "";
 }
 
 function loadToDos(){
+    // if todo exists, show it(paint)
     const toDos = localStorage.getItem(TO_DO_LOCAL_STORAGE);
     if (toDos){
         paintToDo(toDos);
