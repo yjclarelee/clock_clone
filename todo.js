9986f260f6a6bcc7d8616361b6cae156ff3b0990
yjclarelee <yjclarelee@postech.ac.kr>
2020-11-27 07:34:09 +0900

    Save items locally and load existing todos

--- todo.js
+++ todo.js
@@ -2,23 +2,36 @@ const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
+// key value of local storage
 const TO_DO_LOCAL_STORAGE = "toDos";
+
+const toDos = [];
+
+function saveToDos(){
+    // JSON stringify changes a javscript object to a string
+    localStorage.setItem(TO_DO_LOCAL_STORAGE, JSON.stringify(toDos));
+}
 
 function paintToDo(text){
     // can't you just append li without span?
     // span is a container to put the text into
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
+    const newId = toDos.length + 1 ;
     delBtn.innerText = "❌";
     const span = document.createElement("span");
     span.innerText = `${text} `;
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.id = newId;
     toDoList.appendChild(li);
-}
 
-function saveToDo(){
-
+    const toDoObj = {
+        text: text,
+        id: newId
+    }
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -32,10 +45,14 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    // if todo exists, show it(paint)
-    const toDos = localStorage.getItem(TO_DO_LOCAL_STORAGE);
-    if (toDos){
-        paintToDo(toDos);
+    // if todo exists, show it
+    const loadedToDos = localStorage.getItem(TO_DO_LOCAL_STORAGE);
+    if (loadedToDos){
+        // parse the strings to get an object
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        })
     }
 }
 
