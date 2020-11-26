45a49a464409599559f601e606b104bb1f5bd490
yjclarelee <yjclarelee@postech.ac.kr>
2020-11-26 21:02:37 +0900

    Real-time clock with padding of 0 when less than 10

--- clock.js
+++ clock.js
@@ -1,16 +1,21 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
 
+function padZero(num){
+    return num < 10 ? `0${num}` : num;
+}
+
 function setTime(){
     const date = new Date();
     const hour = date.getHours();
     const minute = date.getMinutes();
     const second = date.getSeconds();
-    clockTitle.innerText = `${hour}:${minute}:${second}`;
+    clockTitle.innerText = `${padZero(hour)}:${padZero(minute)}:${padZero(second)}`;
 }
 
 function init(){
-    setTime(); 
+    setTime();
+    setInterval(setTime, 1000); 
 }
 
 init();
